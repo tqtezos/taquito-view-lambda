56e28a7a3ca640fd01b939cf4d85ed91f3e2f3a2
Michael J. Klein <lambdamichael@gmail.com>
2020-01-17 15:53:43 -0500

    working test lambda

--- index.ts
+++ index.ts
@@ -153,6 +153,149 @@ async function view_to_void_lambda({exec_lambda_address, target_contract_address
              { prim: 'PAIR', args: [] } ] ] } ]);
 }
 
+const test_lambda =
+  [ { prim: "PUSH",
+      args: [ { prim: "mutez" }, { int: "0" } ] },
+    { prim: "NONE", args: [ { prim: "key_hash" } ] },
+    { prim: "CREATE_CONTRACT",
+      args:
+        [ [ { prim: "parameter",
+              args: [ { prim: "nat" } ] },
+            { prim: "storage",
+              args: [ { prim: "unit" } ] },
+            { prim: "code",
+              args: [ [ { prim: "FAILWITH" } ] ] } ] ] },
+    { prim: "DIP",
+      args:
+        [ [ { prim: "DIP",
+              args:
+                [ [ { prim: "LAMBDA",
+                      args:
+                        [ { prim: "pair",
+                            args:
+                              [ { prim: "address" },
+                                { prim: "unit" } ] },
+                          { prim: "pair",
+                            args:
+                              [ { prim: "list",
+                                  args:
+                                    [ { prim: "operation" } ] },
+                                { prim: "unit" } ] },
+                          [ { prim: "CAR" },
+                            { prim: "CONTRACT",
+                              args: [ { prim: "nat" } ] },
+                            { prim: "IF_NONE",
+                              args:
+                                [ [ { prim: "PUSH",
+                                      args:
+                                        [ { prim:
+                                              "string" },
+                                          { string:
+                                              "Not 'Tc 'CNat" } ] },
+                                    { prim: "FAILWITH" } ],
+                                  [] ] },
+                            { prim: "PUSH",
+                              args:
+                                [ { prim: "unit" },
+                                  { prim: "Unit" } ] },
+                            { prim: "PAIR" },
+                            { prim: "DIP",
+                              args:
+                                [ [ { prim: "PUSH",
+                                      args:
+                                        [ { prim:
+                                              "address" },
+                                          { string:
+                                              "KT1Rc9HR6dqBf6MxN6cCJjDZ6PidaxvT93U8" } ] },
+                                    { prim: "DUP" },
+                                    { prim: "CONTRACT",
+                                      args:
+                                        [ { prim: "pair",
+                                            args:
+                                              [ { prim:
+                                                  "unit" },
+                                                { prim:
+                                                  "contract",
+                                                  args:
+                                                  [ { prim:
+                                                  "nat" } ] } ] } ] },
+                                    { prim: "IF_NONE",
+                                      args:
+                                        [ [ { prim:
+                                                "FAILWITH" } ],
+                                          [ { prim: "DIP",
+                                              args:
+                                                [ [ { prim:
+                                                  "DROP" } ] ] } ] ] },
+                                    { prim: "PUSH",
+                                      args:
+                                        [ { prim: "mutez" },
+                                          { int: "0" } ] } ] ] },
+                            { prim: "TRANSFER_TOKENS" },
+                            { prim: "DIP",
+                              args:
+                                [ [ { prim: "NIL",
+                                      args:
+                                        [ { prim:
+                                              "operation" } ] } ] ] },
+                            { prim: "CONS" },
+                            { prim: "DIP",
+                              args:
+                                [ [ { prim: "UNIT" } ] ] },
+                            { prim: "PAIR" } ] ] } ] ] },
+            { prim: "APPLY" },
+            { prim: "DIP",
+              args:
+                [ [ { prim: "PUSH",
+                      args:
+                        [ { prim: "address" },
+                          { string:
+                              "KT1NFUsGvAomSSNnKjps8RL1EjGKfWQmM4iw" } ] },
+                    { prim: "DUP" },
+                    { prim: "CONTRACT",
+                      args:
+                        [ { prim: "lambda",
+                            args:
+                              [ { prim: "unit" },
+                                { prim: "pair",
+                                  args:
+                                    [ { prim: "list",
+                                        args:
+                                          [ { prim:
+                                                "operation" } ] },
+                                      { prim: "unit" } ] } ] } ] },
+                    { prim: "IF_NONE",
+                      args:
+                        [ [ { prim: "FAILWITH" } ],
+                          [ { prim: "DIP",
+                              args:
+                                [ [ { prim: "DROP" } ] ] } ] ] },
+                    { prim: "PUSH",
+                      args:
+                        [ { prim: "mutez" },
+                          { int: "0" } ] } ] ] },
+            { prim: "TRANSFER_TOKENS" },
+            { prim: "DIP",
+              args:
+                [ [ { prim: "NIL",
+                      args: [ { prim: "operation" } ] } ] ] },
+            { prim: "CONS" } ] ] }, { prim: "CONS" },
+    { prim: "DIP", args: [ [ { prim: "UNIT" } ] ] },
+    { prim: "PAIR" } ];
+
+async function send_retry(method, args, n=0) {
+  try {
+    console.log(`Retry number ${n}`);
+    return await method.send(args)
+  } catch(err) {
+    if(err.message.match(/Counter \d+ already used for contract/) || err.message.match(/contract\.counter_in_the_past/) || err.message.match(/upstream request timeout/)) {
+      return await send_retry(method, args, n+1)
+    } else {
+      throw err
+    }
+  }
+}
+
 async function main() {
   const { email, password, mnemonic, secret } = JSON.parse(fs.readFileSync('/Users/michaelklein/Downloads/tz1R3vJ5TV8Y5pVj8dicBR23Zv8JArusDkYr.json').toString());
   Tezos.setProvider({ rpc: 'https://api.tez.ie/rpc/babylonnet' });
@@ -162,7 +305,56 @@ async function main() {
                                                       target_contract_address: 'KT1XQKa1N6e9VNLyNbhc6UMEZRJbnAb3wqAg',
                                                       target_contract_parameter: ml2mic('Unit'),
                                                       target_contract_entrypoint: 'getTotalSupply'});
-  p(lambda_parameter);
+/*   p(lambda_parameter); */
+
+
+  /* const target_contract = await Tezos.contract.at('KT1NFUsGvAomSSNnKjps8RL1EjGKfWQmM4iw'); */
+  const target_contract = await Tezos.contract.at('KT1E1trWsE1A9yrbgNeRJC54VCYgEtrbYLSE');
+  /* p('see this'); */
+  /* p(target_contract.methods.main(lambda_parameter)); */
+  /* p('end'); */
+  /* p(target_contract.methods.main); */
+  /* throw Error('hi') */
+  /* p(target_contract); */
+  /* p(target_contract.methods.main(simple_lambda).schema); */
+
+  var resp;
+  try {
+    resp = await send_retry(target_contract.methods.main(test_lambda), { amount: 0 });
+    await resp.confirmation()
+    /* p(resp); */
+    /* const (results : OperationContentsAndResult[]) */
+
+    /* p(resp.results[0].metadata.internal_operation_results.map(x => { */
+    /*   return [typeof x, x?.result, x?.result?.status, typeof x?.result?.status, x?.result?.status == 'failed', x?.result?.status === 'failed']; */
+    /* })); */
+    if (resp.results?.length !== 1) {
+      throw Error('Response results not singleton')
+    }
+    const failed_internal_operations = 
+      resp.results[0]?.metadata?.internal_operation_results?.filter(x => {
+      return x?.result?.status === 'failed';
+    });
+    /* p(failed_internal_operations); */
+    if (failed_internal_operations.length !== 1) {
+      throw Error('Failed internal operaations not singleton')
+    }
+    const failed_internal_operation = failed_internal_operations[0];
+    /* p(failed_internal_operation?.parameters) */
+    p(failed_internal_operation?.parameters?.value)
+
+    /* p(resp.results[0].metadata.internal_operation_results.filter(x => { */
+    /*   return x?.result?.status === 'failed'; */
+    /* })); */
+    /* p(resp.results.length); */
+    /* p(resp.errors); */
+    /* .send(); */
+  } catch(e) {
+    p(e);
+    /* console.log(e.message); */
+  }
+  /* console.log('Response:'); */
+  /* p(resp.body); */
 
 }
 
