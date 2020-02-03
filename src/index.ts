4fb497d6da1c7598ea9a2bb5da56da1de43bcbb4
Philip Diaz <philip@tqgroup.io>
2020-02-03 15:20:28 -0500

    Refactor

---
+++ src/index.ts
@@ -0,0 +1,165 @@
+import fs from "fs";
+import util from "util";
+import { Tezos } from "@taquito/taquito";
+import voidLambda from "./void_lambda";
+
+function log(x: any) {
+  console.log(util.inspect(x, false, null, true /* enable colors */));
+}
+
+async function getEntrypoint(
+  contractAddress: string,
+  entrypointName: string = "default"
+): Promise<object> {
+  const response = await Tezos.rpc.getEntrypoints(contractAddress);
+  const entrypoint = response.entrypoints[entrypointName];
+
+  if (!entrypoint) {
+    throw Error(
+      `Contract ${contractAddress} does not have entrypoint: ${entrypointName}`
+    );
+  }
+
+  return entrypoint;
+}
+
+/* Expected form: */
+/* [ { prim: 'unit' }, */
+/*   { prim: 'contract', args: [ { prim: 'nat' } ] } ] */
+async function getViewEntrypoints(
+  contractAddress: string,
+  entrypointName: string = "default"
+): Promise<[object, object]> {
+  const entrypoint: any = await getEntrypoint(contractAddress, entrypointName);
+
+  if (entrypoint["prim"] !== "pair") {
+    throw Error(
+      `Expected {'prim': 'pair', ..} but found {'prim': ${entrypoint["prim"]}, ..}`
+    );
+  }
+
+  const args = Array.from(entrypoint["args"]) as [any, any];
+
+  if (args.length !== 2) {
+    throw Error(`Expected an Array of length 2, but found: ${args}`);
+  }
+
+  const [parameter, callbackContract] = args;
+
+  if (callbackContract["prim"] !== "contract") {
+    throw Error(
+      `Expected a {prim: 'contract', ...}, but found: ${callbackContract["prim"]}`
+    );
+  }
+
+  if (callbackContract["args"]?.length !== 1) {
+    throw Error(
+      `Expected a single argument to 'contract', but found: ${callbackContract["args"]}`
+    );
+  }
+
+  return [parameter, callbackContract["args"][0]];
+}
+
+async function viewToVoidLambda(
+  execLambdaAddress: string,
+  contractAddress: string,
+  contractParameter: any,
+  contractEntrypoint: string = "default"
+): Promise<object> {
+  const [parameter, callback] = await getViewEntrypoints(
+    contractAddress,
+    contractEntrypoint
+  );
+
+  log([parameter, callback]);
+
+  let contractArgs: any;
+  if (contractEntrypoint === "default") {
+    contractArgs = [
+      { string: `%${contractEntrypoint}` },
+      {
+        prim: "pair",
+        args: [parameter, { prim: "contract", args: [callback] }]
+      }
+    ];
+  } else {
+    contractArgs = [
+      {
+        prim: "pair",
+        args: [parameter, { prim: "contract", args: [callback] }]
+      }
+    ];
+  }
+
+  return voidLambda({
+    callback,
+    parameter,
+    contractParameter,
+    contractAddress,
+    contractArgs,
+    execLambdaAddress
+  });
+}
+
+async function sendRetry(method: any, args: any, n: number = 0): Promise<any> {
+  try {
+    return await method.send(args);
+  } catch (err) {
+    if (
+      err.message.match(/Counter \d+ already used for contract/) ||
+      err.message.match(/contract\.counter_in_the_past/) ||
+      err.message.match(/upstream request timeout/)
+    ) {
+      console.log(`Retry number ${n + 1}`);
+      return await sendRetry(method, args, n + 1);
+    }
+
+    throw err;
+  }
+}
+
+async function main() {
+  Tezos.setProvider({ rpc: "https://api.tez.ie/rpc/babylonnet" });
+
+  const lambdaAddress = "KT1E1trWsE1A9yrbgNeRJC54VCYgEtrbYLSE";
+  const fa12Address = "KT1RUhPAABRhZBctcsWFtymyjpuBQdLTqaAQ";
+
+  const lambdaParameter = await viewToVoidLambda(
+    lambdaAddress,
+    fa12Address,
+    { prim: "Unit" },
+    "getTotalSupply"
+  );
+
+  const contract = await Tezos.contract.at(lambdaAddress);
+
+  // log(contract);
+
+  let resp: any;
+  try {
+    const mainMethod = contract.methods.main(lambdaParameter);
+    resp = await sendRetry(mainMethod, { amount: 0 });
+    await resp.confirmation();
+
+    if (resp.results?.length !== 1) {
+      throw Error("Response results not singleton");
+    }
+    const failedInternalOperations = resp.results[0]?.metadata?.internal_operation_results?.filter(
+      (x: any) => x?.result?.status === "failed"
+    );
+
+    if (failedInternalOperations.length !== 1) {
+      throw Error("Failed internal operaations not singleton");
+    }
+
+    const failedInternalOperation = failedInternalOperations[0];
+
+    log({ result: failedInternalOperation?.parameters?.value });
+    log(resp.results[0]);
+  } catch (e) {
+    log(e);
+  }
+}
+
+main();
---
+++ src/void_lambda.ts
@@ -0,0 +1,172 @@
+interface VoidLambdaParams {
+  callback: object;
+  parameter: object;
+  contractParameter: any;
+  contractAddress: string;
+  contractArgs: any;
+  execLambdaAddress: string;
+}
+
+export default function(params: VoidLambdaParams) {
+  const {
+    callback,
+    parameter,
+    contractParameter,
+    contractAddress,
+    contractArgs,
+    execLambdaAddress
+  } = params;
+
+  return [
+    { prim: "PUSH", args: [{ prim: "mutez" }, { int: "0" }] },
+    { prim: "NONE", args: [{ prim: "key_hash" }] },
+    {
+      prim: "CREATE_CONTRACT",
+      args: [
+        [
+          { prim: "parameter", args: [callback] },
+          { prim: "storage", args: [{ prim: "unit" }] },
+          { prim: "code", args: [[{ prim: "FAILWITH" }]] }
+        ]
+      ]
+    },
+    {
+      prim: "DIP",
+      args: [
+        [
+          {
+            prim: "DIP",
+            args: [
+              [
+                {
+                  prim: "LAMBDA",
+                  args: [
+                    {
+                      prim: "pair",
+                      args: [{ prim: "address" }, { prim: "unit" }]
+                    },
+                    {
+                      prim: "pair",
+                      args: [
+                        { prim: "list", args: [{ prim: "operation" }] },
+                        { prim: "unit" }
+                      ]
+                    },
+                    [
+                      { prim: "CAR" },
+                      { prim: "CONTRACT", args: [callback] },
+                      {
+                        prim: "IF_NONE",
+                        args: [
+                          [
+                            {
+                              prim: "PUSH",
+                              args: [
+                                { prim: "string" },
+                                { string: `Callback type unmatched` }
+                              ]
+                            },
+                            { prim: "FAILWITH" }
+                          ],
+                          []
+                        ]
+                      },
+                      {
+                        prim: "PUSH",
+                        args: [parameter, contractParameter]
+                      },
+                      { prim: "PAIR" },
+                      {
+                        prim: "DIP",
+                        args: [
+                          [
+                            {
+                              prim: "PUSH",
+                              args: [
+                                { prim: "address" },
+                                { string: contractAddress }
+                              ]
+                            },
+                            { prim: "DUP" },
+                            { prim: "CONTRACT", args: contractArgs },
+                            {
+                              prim: "IF_NONE",
+                              args: [
+                                [{ prim: "FAILWITH" }],
+                                [{ prim: "DIP", args: [[{ prim: "DROP" }]] }]
+                              ]
+                            },
+                            {
+                              prim: "PUSH",
+                              args: [{ prim: "mutez" }, { int: "0" }]
+                            }
+                          ]
+                        ]
+                      },
+                      { prim: "TRANSFER_TOKENS" },
+                      {
+                        prim: "DIP",
+                        args: [[{ prim: "NIL", args: [{ prim: "operation" }] }]]
+                      },
+                      { prim: "CONS" },
+                      { prim: "DIP", args: [[{ prim: "UNIT" }]] },
+                      { prim: "PAIR" }
+                    ]
+                  ]
+                }
+              ]
+            ]
+          },
+          { prim: "APPLY" },
+          {
+            prim: "DIP",
+            args: [
+              [
+                {
+                  prim: "PUSH",
+                  args: [{ prim: "address" }, { string: execLambdaAddress }]
+                },
+                { prim: "DUP" },
+                {
+                  prim: "CONTRACT",
+                  args: [
+                    {
+                      prim: "lambda",
+                      args: [
+                        { prim: "unit" },
+                        {
+                          prim: "pair",
+                          args: [
+                            { prim: "list", args: [{ prim: "operation" }] },
+                            { prim: "unit" }
+                          ]
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  prim: "IF_NONE",
+                  args: [
+                    [{ prim: "FAILWITH" }],
+                    [{ prim: "DIP", args: [[{ prim: "DROP" }]] }]
+                  ]
+                },
+                { prim: "PUSH", args: [{ prim: "mutez" }, { int: "0" }] }
+              ]
+            ]
+          },
+          { prim: "TRANSFER_TOKENS" },
+          {
+            prim: "DIP",
+            args: [[{ prim: "NIL", args: [{ prim: "operation" }] }]]
+          },
+          { prim: "CONS" }
+        ]
+      ]
+    },
+    { prim: "CONS" },
+    { prim: "DIP", args: [[{ prim: "UNIT" }]] },
+    { prim: "PAIR" }
+  ];
+}
